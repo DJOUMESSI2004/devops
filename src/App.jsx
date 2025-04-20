90ec09e8b6a3dd382dd9acc45835c71793aa8063
wilfrid <wilfrid.ndongmodjoumessi@fanuc.eu>
2025-04-20 16:03:22 +0200

    edit app

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <h1>Hello World React + Vite</h1>
-      <h2>I Love DevOps</h2>
+      {/* <h2>I Love DevOps</h2> */}
       <h2>Papa Je T'aime</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa provident cupiditate dolore suscipit veniam temporibus saepe architecto unde, quia ab. Distinctio aliquam odit fugit totam placeat laboriosam. Omnis, alias laboriosam.</p>
     </>
