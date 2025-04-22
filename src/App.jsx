1abd21f568b642a12f5d29011f4098b12a5b66e8
wilfrid <wilfrid.ndongmodjoumessi@fanuc.eu>
2025-04-22 10:08:29 +0200

    added text

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,9 @@ function App() {
       <h1>Hello World React + Vite</h1>
       <h2>Papa Je T'aime</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa provident cupiditate dolore suscipit veniam temporibus saepe architecto unde, quia ab. Distinctio aliquam odit fugit totam placeat laboriosam. Omnis, alias laboriosam.</p>
+   
+      <h2>I love Scholling</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus ab ducimus obcaecati odio eaque laudantium, quaerat vitae inventore nostrum commodi sunt nesciunt labore nobis illo mollitia sequi? Eos, voluptatem dolor.</p>
     </>
   )
 }
